f8fe9706ecc8e221833fc2bdee42949d0a9b719e
Meiji Murazaki <mehdi.benadel@gmail.com>
2017-08-05 00:30:01 +0200

    Fixing error at first build and run

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
-import {NestFactory} from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app/app.module';
 
 const app = NestFactory.create(ApplicationModule);
-
-app.listen(3000, () => console.log('Application is listening on port 3000.'));
+app.then(instance => instance.listen(3000, () => console.log('Application is listening on port 3000')));
